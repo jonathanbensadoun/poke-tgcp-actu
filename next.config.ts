f306eea8dba87ba097fdd53ecc355ae7026fa279
jonathanBensadoun <jonathan.ben-sadoun@oclock.school>
2024-11-20 16:59:00 +0100

    add next

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
+  output: "export", // Exporte un site statique
   images: {
-    domains: ["localhost", "poke-tgcp-actu.netlify.app"],
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "X-XSS-Protection",
-            value: "1; mode=block",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "same-origin",
-          },
-        ],
-      },
-    ];
+    unoptimized: true, // Désactive l'optimisation d'image (compatible Netlify)
   },
 };
 
