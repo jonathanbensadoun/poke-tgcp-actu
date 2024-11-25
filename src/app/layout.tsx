671f0122a6e107d522bb3a3953a686f723733767
jonathanBensadoun <jonathan.ben-sadoun@oclock.school>
2024-11-25 22:02:27 +0100

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Fredoka } from "next/font/google";
+import Script from "next/script";
 
 const fredoka = Fredoka({
   subsets: ["latin"],
@@ -62,6 +63,19 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <head>
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KHRC7L26');
+            `,
+          }}
+        />
         <title>Pokémon TCGP Actu - Actualités et Stratégies</title>
         <meta
           name="description"
@@ -73,6 +87,15 @@ export default function RootLayout({
         />
       </head>
       <body className={`${fredoka.className}  antialiased bg-[#3d2368] `}>
+        {/* Noscript fallback */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KHRC7L26"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <header>
           <Navbar />
           <Header />
