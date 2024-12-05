54a36552bf224ac9b9477c9604e9a08bd1a2bd07
jonathanBensadoun <jonathan.ben-sadoun@oclock.school>
2024-12-05 16:16:22 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,6 +22,7 @@ export default function Home() {
     }
   };
   const summary = [
+    "Grodoudou Amphinobi, j’endors et tu tapes !",
     "Pikachu à l'honneur dans le nouveau pass premium",
     "Grodoudou Smogogo, le duo des afflictions",
     "Top deck pour battre l’évènement Florizarre !",
